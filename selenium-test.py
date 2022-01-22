259917225b60324e001ac0b230d15dc81a4c8114
NJWilly <williamcnoel@gmail.com>
2022-01-21 19:46:53 -0500

    selenium test is now working

--- selenium-test.py
+++ selenium-test.py
@@ -1,10 +1,11 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Chrome()
 driver.get("http://www.python.org")
 assert "Python" in driver.title
-elem = driver.find_element("q")
+elem = driver.find_element(By.NAME, "q")
 elem.clear()
 elem.send_keys("pycon")
 elem.send_keys(Keys.RETURN)
