bb10dd9f124dc356cee8e7f300f1b002c47de766
NJWilly <williamcnoel@gmail.com>
2022-01-22 14:29:12 -0500

    code retrieved and merged from dell laptop

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
-# This is a sample Python script.
+# This script should display status summary of network printers listed in a CSV file
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import csv
+import requests
+from bs4 import BeautifulSoup
 
+headers = {"User-agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                         'Chrome/97.0.4692.71 Safari/537.36'}
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    with open('NetworkPrinters.csv', newline='') as f:
+        reader = csv.reader(f)
+        network_printers = list(reader)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    for network_printer in network_printers[1:]:
+        if len(network_printer[3]) != 0:
+            try:
+                page = requests.get(network_printer[3] + 'web/guest/en/websys/webArch/mainFrame.cgi', headers=headers)
+            except requests.exceptions.RequestException as error:
+                # print("Error: ", error)
+                print(f'No response from printer at {network_printer[3]},'
+                      f' go to Ackerson Hall, Room {network_printer[0]} to check it out')
+            else:
+                if page.status_code == 200:
+                    soup = BeautifulSoup(page.content, 'lxml')
+                    # status = soup.find('dd', class_='word-wrap').get_text()
+                    title = soup.find('title').get_text()
+                    print(f'Printer {title.split()[0]} was found')
