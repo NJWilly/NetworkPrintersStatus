1f9125723b1e9516c7f5b3b50e8e006d169523b2
NJWilly <williamcnoel@gmail.com>
2022-01-24 13:49:44 -0500

    runs

--- selenium-test.py
+++ selenium-test.py
@@ -1,13 +1,35 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
+import csv
+
+from urllib3.exceptions import NewConnectionError, MaxRetryError
 
 driver = webdriver.Chrome()
-driver.get("http://www.python.org")
-assert "Python" in driver.title
-elem = driver.find_element(By.NAME, "q")
-elem.clear()
-elem.send_keys("pycon")
-elem.send_keys(Keys.RETURN)
-assert "No results found." not in driver.page_source
-driver.close()
+
+with open('NetworkPrinters.csv', newline='') as f:
+    reader = csv.reader(f)
+    network_printers = list(reader)
+
+for network_printer in network_printers[1:]:
+    if len(network_printer[3]) != 0:
+        print(network_printer[3], end=' ')
+        try:
+            driver.get(network_printer[3])
+            # driver.implicitly_wait(3)
+            # elem = driver.find_element(By.TAG_NAME, "title")
+            print("success")
+            # print(elem.text)
+        except NewConnectionError:
+            print(f'Error connecting to {network_printer[3]}')
+            continue
+        except MaxRetryError:
+            print("max retry error")
+            continue
+        except ConnectionRefusedError:
+            print("Connection Refused Error")
+            continue
+        except:
+            print("other Error")
+
+driver.quit()
+# driver.close()
